ff43601965d015dd8841b9d299d3a62420ad3532
bryana08 <bryana.nongni@facsciences-uy1.cm>
2026-05-15 19:44:45 +0100

    Add redirect from root to login page

--- factureblockchain/next.config.ts
+++ factureblockchain/next.config.ts
@@ -5,3 +5,14 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: false,
+      },
+    ]
+  },
+}
