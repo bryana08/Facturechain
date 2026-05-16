72b4ad8c8246035b1214bc70c31ccf27a49fed3b
bryana08 <bryana.nongni@facsciences-uy1.cm>
2026-05-16 06:38:23 +0100

    Redirect to login page on Home component

--- factureblockchain/app/page.tsx
+++ factureblockchain/app/page.tsx
@@ -1,27 +1,5 @@
-import CtaSection from "../src/components/CtaSection";
-import BlockchainSection from "../src/components/BlockchainSection";
-import Hero from "../src/components/Hero";
-import HowItWorksSection from "../src/components/HowItWorksSection";
-import ImpactSection from "../src/components/ImpactSection";
-import ProblemSection from "../src/components/ProblemSection";
-import SolutionSection from "../src/components/SolutionSection";
-import StatsBand from "../src/components/StatsBand";
+import { redirect } from 'next/navigation'
 
-export default function HomePage() {
-  return (
-    <div className="space-y-16 pb-20">
-      <Hero />
-      <StatsBand />
-      <ProblemSection />
-      <SolutionSection />
-      <HowItWorksSection />
-      <BlockchainSection />
-      <ImpactSection />
-      <CtaSection />
-    </div>
-    import { redirect } from 'next/navigation'
 export default function Home() {
   redirect('/login')
-}
-  );
 }
